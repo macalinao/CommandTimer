25085c4cf58d4081d9a42b86851fe80240d54aa9
Ian Macalinao <ianmacalinao@gmail.com>
2013-07-21 04:51:03 -0500

    Load messages from config
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

--- src/main/java/net/new_liberty/commandtimer/CommandTimer.java
+++ src/main/java/net/new_liberty/commandtimer/CommandTimer.java
@@ -1,9 +1,38 @@
 package net.new_liberty.commandtimer;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Command Timer.
  */
 public class CommandTimer extends JavaPlugin {
+    private Map<String, String> messages = new HashMap<String, String>();
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
+
+        ConfigurationSection messagesSection = config.getConfigurationSection("messages");
+        if (messagesSection != null) {
+            for (Entry<String, Object> msg : messagesSection.getValues(false).entrySet()) {
+                messages.put(msg.getKey(), msg.getValue().toString());
+            }
+        }
+    }
+
+    /**
+     * Gets the default message.
+     *
+     * @param key
+     * @return
+     */
+    public String getMessage(String key) {
+        return messages.get(key);
+    }
 }
